ca3177c4f4406c753f1f4e9d21dea0dc3f6face7
pedro henrique <pedro-hsb>
2024-02-17 22:55:59 -0300

    projeto de sacola do ifood

--- src/main/java/com/sacola/sacolaapi/SacolaApiApplication.java
+++ src/main/java/com/sacola/sacolaapi/SacolaApiApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SacolaApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SacolaApiApplication.class, args);
+		System.out.println("teste");
 	}
 
 }
